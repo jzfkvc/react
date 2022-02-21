e2f8027b30232d9e161a974a007ffe8badb13475
jzfkvc <80673922+jzfkvc@users.noreply.github.com>
2022-02-21 19:47:33 +0100

    Customer initial commit

---
+++ react-app/src/Customer.js
@@ -0,0 +1,50 @@
+import { Box, DataTable } from "grommet";
+import { useQuery, gql } from "@apollo/client";
+
+const GET_CUSTOMER = gql`
+  query getCustomer {
+    customers {
+      id
+      name
+      birthDate
+      vip
+      spendedMoney
+    }
+  }
+`;
+
+function Customers() {
+  const { loading, error, data } = useQuery(GET_CUSTOMER);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return (
+    <div>
+      <Box
+        fill="vertical"
+        overflow="auto"
+        align="center"
+        flex="grow"
+        justify="center"
+        direction="column"
+        pad="xsmall"
+      >
+        <DataTable
+          columns={[
+            { header: "Id", property: "id", primary: true },
+            { header: "Name", property: "name" },
+            { property: "birthDate", header: "BirthDate" },
+            { property: "vip", header: "Vip" },
+            { property: "spendedMoney", header: "SpendedMoney" },
+          ]}
+          data={data.customers}
+          fill="horizontal"
+          onClickRow={() => {}}
+        />
+      </Box>
+    </div>
+  );
+}
+
+export default Customers;
